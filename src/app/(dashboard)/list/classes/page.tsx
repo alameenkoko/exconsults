dbb4489375a95ceba3030b01240cd57ef294a899
alameenkoko <alameenkoko5@gmail.com>
2025-05-29 19:11:56 +0100

    Update

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -55,15 +56,12 @@ const ClassListPage = () => {
      
        <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/edit.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
+          
           {role === "admin" && (
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaRed">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
+            <>
+            <FormModal table="class" type="update" data={item} />
+            <FormModal table="class" type="delete" id={item.id} />
+            </>
           )}
         </div>
       </td>
@@ -85,9 +83,7 @@ const ClassListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaRed">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
+             <FormModal table="teacher" type="create" />
             )}
           </div>
         </div>
--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -40,15 +41,12 @@ const SubjectListPage = () => {
      
        <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
+           
           {role === "admin" && (
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaRed">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
+            <>
+            <FormModal table="subject" type="update" data={item} />
+            <FormModal table="subject" type="delete" id={item.id} />
+            </>
           )}
         </div>
       </td>
@@ -69,11 +67,7 @@ const SubjectListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaRed">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaRed">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
-            )}
+            {role === "admin" && <FormModal table="teacher" type="create"/>}
           </div>
         </div>
       </div>
