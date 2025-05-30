2ca4d956e4445dd21cf82fae4c980a1844137280
alameenkoko <alameenkoko5@gmail.com>
2025-05-30 16:32:54 +0100

    I added inputs also include Bood Type Input

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -18,6 +18,7 @@ const schema = z.object({
   lastName:z.string().min(1,{message:"Last name is required!"}),
   phone:z.string().min(1,{message:"Phone number is required!"}),
   address:z.string().min(1,{message:"Address is required!"}),
+  bloodType:z.string().min(1,{message:"Blood type is required!"}),
   birthday:z.date({message:"Birthday is required!"}),
   sex:z.enum(["male","female"], {message:"Sex is required!"}),
   img:z.instanceof(File,{message:"Image is required!"}),
@@ -56,7 +57,95 @@ const TeacherForm = ({
        register={register} 
        error={errors.username}
        />
-       <span className='text-xs text-gray-400 font-medium'>Personal Information</span>
+        <InputField 
+       label="Email" 
+       name="email" 
+       type='email'
+       defaultValue={data?.email} 
+       register={register} 
+       error={errors.email}
+       />
+        <InputField 
+       label="Password" 
+       name="password" 
+       type='password'
+      defaultValue={data?.password} 
+       register={register} 
+       error={errors.password}
+       />
+       <span className='text-xs text-gray-400 font-medium'>
+        Personal Information
+        </span>
+        <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="First Name"
+          name="name"
+          defaultValue={data?.name}
+          register={register}
+          error={errors.name}
+        />
+        <InputField
+          label="Last Name"
+          name="surname"
+          defaultValue={data?.surname}
+          register={register}
+          error={errors.surname}
+        />
+        <InputField
+          label="Phone"
+          name="phone"
+          defaultValue={data?.phone}
+          register={register}
+          error={errors.phone}
+        />
+        <InputField
+          label="Address"
+          name="address"
+          defaultValue={data?.address}
+          register={register}
+          error={errors.address}
+        />
+        <InputField
+          label="Blood Type"
+          name="bloodType"
+          defaultValue={data?.bloodType}
+          register={register}
+          error={errors.bloodType}
+        />
+        <InputField
+          label="Birthday"
+          name="birthday"
+          defaultValue={data?.birthday.toISOString().split("T")[0]}
+          register={register}
+          error={errors.birthday}
+          type="date"
+        />
+        {data && (
+          <InputField
+            label="Id"
+            name="id"
+            defaultValue={data?.id}
+            register={register}
+            error={errors?.id}
+            hidden
+          />
+        )}
+        <div className="flex flex-col gap-2 w-full md:w-1/4">
+          <label className="text-xs text-gray-500">Sex</label>
+          <select
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("sex")}
+            defaultValue={data?.sex}
+          >
+            <option value="MALE">Male</option>
+            <option value="FEMALE">Female</option>
+          </select>
+          {errors.sex?.message && (
+            <p className="text-xs text-red-400">
+              {errors.sex.message.toString()}
+            </p>
+          )}
+        </div>
      <button 
      className='bg-blue-400 text-white p-2 rounded-md'>
      {type === "create" ? "Create" : "Update"  }
