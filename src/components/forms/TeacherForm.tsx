63e90b22044ba1113e726656b87cf0b8173d8a7d
alameenkoko <alameenkoko5@gmail.com>
2025-05-30 18:05:00 +0100

    Added Inputs

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import InputField from '../InputField';
+import Image from 'next/image';
 
 const schema = z.object({
   username: z
@@ -46,10 +47,11 @@ const TeacherForm = ({
   });
     return ( 
     <form className="flex flex-col gap-8" onSubmit={onSubmit}>
-        <h1 className='text-xl font-semibold'>Create a new lecturer</h1>
+        <h1 className='text-xl font-semibold'>Create a New Lecturer</h1>
         <span className='text-xs text-gray-400 font-medium'>
         Authentication Information
         </span>
+       <div className="flex justify-between flex-wrap gap-4">
        <InputField 
        label="Username" 
        name="username" 
@@ -73,23 +75,24 @@ const TeacherForm = ({
        register={register} 
        error={errors.password}
        />
+       </div>
        <span className='text-xs text-gray-400 font-medium'>
         Personal Information
         </span>
         <div className="flex justify-between flex-wrap gap-4">
         <InputField
           label="First Name"
-          name="name"
-          defaultValue={data?.name}
+          name="firstName"
+          defaultValue={data?.firstName}
           register={register}
-          error={errors.name}
+          error={errors.firstName}
         />
         <InputField
           label="Last Name"
-          name="surname"
-          defaultValue={data?.surname}
+          name="lastName"
+          defaultValue={data?.lastName}
           register={register}
-          error={errors.surname}
+          error={errors.lastName}
         />
         <InputField
           label="Phone"
@@ -120,32 +123,32 @@ const TeacherForm = ({
           error={errors.birthday}
           type="date"
         />
-        {data && (
-          <InputField
-            label="Id"
-            name="id"
-            defaultValue={data?.id}
-            register={register}
-            error={errors?.id}
-            hidden
-          />
+
+         <div className='flex flex-col gap-2 w-full md:w-1/4'>
+        <label className='text-xs text-gray-500'>Sex</label>
+        <select className='ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full'{...register("sex")} defaultValue={data?.sex}>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        </select>
+        {errors.sex?.message && ( 
+            <p className='text-xs text-red-400 font-medium'>
+              {errors.sex.message.toString()}</p>
         )}
-        <div className="flex flex-col gap-2 w-full md:w-1/4">
-          <label className="text-xs text-gray-500">Sex</label>
-          <select
-            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
-            {...register("sex")}
-            defaultValue={data?.sex}
-          >
-            <option value="MALE">Male</option>
-            <option value="FEMALE">Female</option>
-          </select>
-          {errors.sex?.message && (
-            <p className="text-xs text-red-400">
-              {errors.sex.message.toString()}
-            </p>
-          )}
         </div>
+          <div className='flex flex-col gap-2 w-full md:w-1/4 justify-center'>
+        <label className='text-xs text-gray-500 flex items-center gap-2 cursor-pointer' htmlFor='img'>
+          <Image src="/upload.png" alt="" width={32} height={32}/>
+          <span>Upload Image</span>
+        </label>
+        <input type='file' id='img' {...register("img")} className='hidden'/>
+        
+        
+        {errors.sex?.message && ( 
+            <p className='text-xs text-red-400 font-medium'>
+              {errors.sex.message.toString()}</p>
+        )}
+        </div>
+         </div>
      <button 
      className='bg-blue-400 text-white p-2 rounded-md'>
      {type === "create" ? "Create" : "Update"  }
